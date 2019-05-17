bac7865bf28854efa6f82e61b464ce00d407b6f9
A Machalica <a.machalica@selleo.com>
2019-05-17 21:03:26 +0200

    Updates

---
+++ precache-manifest.02d7de4adbeef7929c1e2f78babfb844.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b3a1cfea22cb050aecbc1912a3a2f9",
+    "url": "/strava_stats/index.html"
+  },
+  {
+    "revision": "05f17decd6b7b74c997f",
+    "url": "/strava_stats/static/css/main.cb0cc02e.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/strava_stats/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "05f17decd6b7b74c997f",
+    "url": "/strava_stats/static/js/main.be9ed112.chunk.js"
+  },
+  {
+    "revision": "f7862d77ac5b8b66b272",
+    "url": "/strava_stats/static/js/runtime~main.c5e026d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/strava_stats/static/media/logo.5d5d9eef.svg"
+  }
+]);
