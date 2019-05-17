98ca23442e174d48be65947463c58d578671a4ad
A Machalica <a.machalica@selleo.com>
2019-05-18 00:10:07 +0200

    Add activities fetching and display

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component}from 'react';
 import './App.css';
+import ActivityCard from "./components/ActivityCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    activities: [],
+  };
+
+  componentDidMount() {
+    const activities = JSON.parse(localStorage.getItem('activities'));
+    if (activities) {
+      this.setState({activities});
+    } else {
+      this.fetchActivities()
+    }
+  }
+
+  fetchActivities() {
+    fetch("https://www.strava.com/api/v3/athlete/activities?page=1&per_page=40", {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': "Bearer [[TOKEN]]",
+      },
+    })
+      .then(response => {
+        response.json().then((data) => {
+          const activities = data.map((activity) => ({
+            id: activity.id,
+            type: activity.type,
+            name: activity.name,
+            distance: activity.distance,
+            startDate: activity.start_date,
+            startDateLocal: activity.start_date_local,
+            totalElevationGain: activity.total_elevation_gain,
+            maxSpeed: activity.max_speed,
+            movingTime: activity.moving_time,
+            kudosCount: activity.kudos_count,
+            averageSpeed: activity.average_speed,
+          }));
+          localStorage.setItem('activities', JSON.stringify(activities));
+          this.setState({activities})
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Your strava activities
+        </header>
+        <div className="activities__container">
+          {this.state.activities && this.state.activities.map((activity) => (
+            <ActivityCard activity={activity} key={activity.id}/>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
